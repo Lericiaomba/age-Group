a24279f7143f61c11b79d1659fc9653a78ae9a81
Lericiaomba <lericiaomba99@gmail.com>
2023-08-17 11:04:29 +0300

    first commit

---
+++ Age-Group/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BirthYear = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // BirthYear
+            // 
+            this.BirthYear.BackColor = System.Drawing.Color.Yellow;
+            this.BirthYear.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.BirthYear.ForeColor = System.Drawing.Color.DarkRed;
+            this.BirthYear.Location = new System.Drawing.Point(358, 28);
+            this.BirthYear.Name = "BirthYear";
+            this.BirthYear.Size = new System.Drawing.Size(218, 44);
+            this.BirthYear.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(25, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(313, 33);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Enter Your BirthYear:";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.ForeColor = System.Drawing.Color.DarkGreen;
+            this.button1.Location = new System.Drawing.Point(31, 108);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(545, 63);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Show My Age";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.Color.DarkRed;
+            this.label2.Location = new System.Drawing.Point(98, 206);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(387, 33);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Your Age will Appear here!";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.ForeColor = System.Drawing.Color.DarkGreen;
+            this.button2.Location = new System.Drawing.Point(31, 277);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(545, 63);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Show My Age Group";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.Color.DarkRed;
+            this.label3.Location = new System.Drawing.Point(56, 380);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(482, 33);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Your Age Group will Appear here!";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightBlue;
+            this.ClientSize = new System.Drawing.Size(606, 466);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BirthYear);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox BirthYear;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Age-Group/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        int Age;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int BY = 0;
+            try
+            {
+                BY = Convert.ToInt32(BirthYear.Text);
+            }
+            catch
+            {
+                MessageBox.Show("The Birth Year must be a Number!");
+                return;
+            }
+            if (BY > DateTime.Now.Year)
+            {
+                MessageBox.Show("Wrong Birth Year! Try Again");
+                return;
+            }
+            Age=DateTime.Now.Year-BY;
+            label2.Text = "You Are "+Age+" Years OLD";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            String AgeGroup;
+            if (Age > 60) AgeGroup = "OLD";
+            else if (Age > 45) AgeGroup = "Middle-Age";
+            else if (Age > 19) AgeGroup = "Young";
+            else if (Age > 12) AgeGroup = "Teenager";
+            else if (Age >= 0) AgeGroup = "Child";
+            else AgeGroup = "Not entered";
+            label3.Text = "Your Age Group is: " + AgeGroup;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("This program shows your Age & Age Group, Would you like to continue?", "Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.No)
+                Application.Exit();
+        }
+    }
+}
